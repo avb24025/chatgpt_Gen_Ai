fad6206215dd4d3dc780efd1d4275177540c2893
avb24025 <avb24022005@gmail.com>
2025-10-21 20:36:07 +0530

    updated backend

--- Backend/index.js
+++ Backend/index.js
@@ -9,11 +9,7 @@ const app=express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173", // <- frontend URL
-  methods: ["GET", "POST", "OPTIONS"],
-  credentials: true
-}));
+app.use(cors());
 
 app.post('/api/chat', async (req, res) => {
     const { prompt, sid} = req.body;
